b0815bc8f8653b84e59ad7e531078b47b7c2e53a
in127278 <dovah@interia.eu>
2017-12-06 16:04:44 +0100

    first commit

---
+++ src/main/java/com/sample/ClassicCar.java
@@ -0,0 +1,28 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class ClassicCar {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	kSession.fireAllRules();
+            // go !
+            
+            //kSession.insert(message);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+
+}
